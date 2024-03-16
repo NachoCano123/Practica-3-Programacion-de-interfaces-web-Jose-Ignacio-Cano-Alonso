02cae336516da8a5f7527a0e9c18aa4a5c424a0c
NachoCano123 <145673920+NachoCano123@users.noreply.github.com>
2024-03-16 15:43:41 +0100

    Update Button.tsx

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,12 +1,27 @@
-import { JSX } from "preact";
-import { IS_BROWSER } from "$fresh/runtime.ts";
+import { FunctionComponent } from "https://esm.sh/v128/preact@10.19.2/src/index.js";
+import { JSX } from "preact/jsx-runtime";
 
-export function Button(props: JSX.HTMLAttributes<HTMLButtonElement>) {
-  return (
-    <button
-      {...props}
-      disabled={!IS_BROWSER || props.disabled}
-      class="px-2 py-1 border-gray-500 border-2 rounded bg-white hover:bg-gray-200 transition-colors"
-    />
-  );
+type PropsComponenteBoton = JSX.HTMLAttributes<HTMLButtonElement> & {
+  variant: "Solicitud" | "Guardar" | "Mensaje"
 }
+
+export function Button(props: PropsComponenteBoton) {
+  return(
+    <button 
+    {...props}
+    class={`text- ${props.variant}`}
+    />
+  )
+}
+
+const Miboton: FunctionComponent<PropsComponenteBoton> = (props) => {
+  const { variant, children, ...rest} = props
+
+  return (
+    <button class={`miboton ${variant}`} {...rest}>
+    {children}
+    </button>
+  )
+}
+
+export default Miboton
