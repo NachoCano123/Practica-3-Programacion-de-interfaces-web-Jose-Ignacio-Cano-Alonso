5385687586241ab61777f3d0091b55d9e239711a
NachoCano123 <145673920+NachoCano123@users.noreply.github.com>
2024-03-16 15:44:35 +0100

    Update Empleo.tsx

--- components/Empleo.tsx
+++ components/Empleo.tsx
@@ -1,5 +1,6 @@
 import { FunctionComponent } from "preact";
 import { Job } from "../types.tsx"
+import { Button } from "./Button.tsx";
 
 const Trabajo: FunctionComponent<Job> = (props) => {
     const {slug, company_name, title, description, remote, url, job_types, location, created_at} = props
@@ -12,7 +13,14 @@ const Trabajo: FunctionComponent<Job> = (props) => {
             <a href={url}> Mi perfil </a>
             <hr></hr>
             <p> Tipos de trabajo: {job_types} </p>
-            <h2> Acerca del empleo </h2>
+            <Button variant = "Solicitud" role="link" onClick={() => <a href={url}/>}> Solicitar </Button> 
+            <Button variant = "Guardar"> Guardar </Button>
+            <hr></hr>
+            <h2>Conoce al equipo de contratación</h2>
+            <image class="imagen" src="https://www.labsaenzrenauld.com/wp-content/uploads/2020/10/Perfil-hombre-ba%CC%81sico_738242395.jpg"/>
+            <Button variant = "Mensaje"> Mensaje </Button>
+            <hr></hr>
+            <h3> Acerca del empleo </h3>
             <p dangerouslySetInnerHTML={{ __html: description }}> </p>
             <p> Creación de la página: {created_at} </p>
         </div>
